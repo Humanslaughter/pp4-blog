4444c03503a6eb25bd482176eaca9d07556a5d6f
Humanslaughter <theodorewikstrom@hotmail.com>
2023-12-15 05:39:46 +0000

    Add filter posted on to Admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('post_title', 'post_slug', 'post_status')
-    search_fields = ['post_title']
-    list_filter = ('post_status',)
+    search_fields = ['post_title', 'post_content']
+    list_filter = ('post_status', 'posted_on',)
     prepopulated_fields = {'post_slug': ('post_title',)}
     summernote_fields = ('post_content',)
 
