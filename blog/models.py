1bde6d9d18a1677583e79de39503a879ee4f35d8
Humanslaughter <theodorewikstrom@hotmail.com>
2023-12-15 02:07:11 +0000

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    blogger = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    post_title = models.CharField(max_length=200, unique=True)
+    post_slug = models.SlugField(max_length=200, unique=True)
+    post_content = models.TextField()
+    post_status = models.IntegerField(choices=STATUS, default=0)
+    posted_on = models.DateTimeField(auto_now_add=True)
