41a6d458972a580acc2066af7c33ed692630b7db
Humanslaughter <theodorewikstrom@hotmail.com>
2023-12-17 01:45:48 +0000

    Enable serving of image files

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -7,6 +8,7 @@ class Post(models.Model):
     blogger = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     post_title = models.CharField(max_length=200, unique=True)
     post_slug = models.SlugField(max_length=200, unique=True)
+    post_image = CloudinaryField('image', default='placeholder')
     post_content = models.TextField()
     post_status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
