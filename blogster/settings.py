a46d80fbe46517e6d8f282f2524272cf49f8737f
Humanslaughter <theodorewikstrom@hotmail.com>
2023-12-18 23:16:07 +0000

    Fix migration issue

--- blogster/settings.py
+++ blogster/settings.py
@@ -16,9 +16,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-humanslaughter-pp4blog-y9l65fl6419.ws-us107.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-humanslaughter-pp4blog-jc23ydy4hwi.ws-us107.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
