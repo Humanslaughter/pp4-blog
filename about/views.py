952a114198a533462890ba2975082b451e08cdb7
Humanslaughter <theodorewikstrom@hotmail.com>
2023-12-20 06:57:17 +0000

    CI Python Linter

--- about/views.py
+++ about/views.py
@@ -10,8 +10,10 @@ def about_me(request):
         contact_form = ContactForm(data=request.POST)
         if contact_form.is_valid():
             contact_form.save()
-            messages.add_message(request, messages.SUCCESS, 
-            "Collaboration request sent. I'll respond to you as soon as possible!")
+            messages.add_message(
+                request,
+                messages.SUCCESS,
+                'Collaboration request sent. I will respond to you soon!')
 
     about = About.objects.all().order_by('-updated_on').first()
     contact_form = ContactForm()
